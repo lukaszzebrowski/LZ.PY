d983c539ae3e33df9dbf7c1fcab7c4243d75690e
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-02-26 11:07:19 +0100

    typy danych

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
 import sys
+import math
 
+g='Wizualizacja danych'
+h=' grupa 3'
+print(g+h)
+#komentarz
+#TODO: aa
+#FIXME: aaa
+
+a = 'isi\ngrupa niestacjonarna'
 
 print(sys.version)
+
 print('Hello World!')
-print(4%2)
+
+print(a)
+print(type(a))
+
+b=5
+print(b)
+
+c=5.4
+print(c)
+
+d=3+2j
+print(d)
+print(type(d))
+
+print(4**2)
+e=7
+f=3
+print(e//f)
+print(2/3**f)
+
+print(math.pow(e,f))
+
+
+
+
