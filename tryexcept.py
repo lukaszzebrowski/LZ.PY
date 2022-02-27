7af2a6d04d0c0c118913c0c25acb363e386f5110
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-02-27 14:30:15 +0100

    tryexcept

--- tryexcept.py
+++ tryexcept.py
@@ -3,3 +3,16 @@
 #-blok instruckji
 #except nazwa_bledu
 #-blok instrukcji po napotkaniu bledu
+
+liczba1=input('Podaj pierwszą liczbę: ')
+liczba2=input('Podaj drugą liczbę: ')
+
+try:
+    liczba1=int(liczba1)
+    liczba2=int(liczba2)
+    wynik=liczba1/liczba2
+    print('Winik działania = '+str(wynik))
+except ZeroDivisionError:
+    print('Nie można dzielić przez 0.!!!')
+except ValueError:
+    print('Nie wczytano liczby całkowitej!!!')
