5d7e6433b9962762fae605c5f20bd4f3cbfaf134
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-02-27 14:50:18 +0100

    lista i słownik

---
+++ listyislowniki.py
@@ -0,0 +1,14 @@
+#lista=[] nawiasy kwadratowe, moga byc rozne typy, lancuchy, inna lista, int, float
+#slownik={} nawiasy klamrowe, (klucz wartosc)<==jeden element, jest unikatowy
+#słownik={1:10, 'klucz':'wartosc', 1:20}
+#słownik['klucz']
+
+słownik={'key1':112244,'key2':335566} # utworzenie slownika
+print(słownik['key1'], słownik['key2']) # wyswietlenie calego słownika
+del słownik['key1'] #usuniecie key1
+print(słownik) #sprawdzenie czy usuniety
+słownik['key2'] = 'nowa wartosc' # nadanie nowej wartosci
+print(słownik) #sprawdzenie wartosci
+
+
+
