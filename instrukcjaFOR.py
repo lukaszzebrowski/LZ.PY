c8b27fba9b42da190f2c0917283373932a8e3844
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-02-27 13:42:43 +0100

    instrukcje warunkowe for

---
+++ instrukcjaFOR.py
@@ -0,0 +1,10 @@
+# for licznik in sekwencja
+#     instruckja lub blok instrukcji
+#
+# else:
+# instruckje po zakonczeniu dzialania petli
+#
+# range(start(i=0),stop(i<>0)>step(i++))
+
+for i in range(0, 6, 1):
+    print(i)
