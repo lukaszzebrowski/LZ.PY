c472b5945c50e3fab6bbb7224c3d82e112807204
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-02-27 14:55:42 +0100

    lista i słownik

--- listyislowniki.py
+++ listyislowniki.py
@@ -3,12 +3,20 @@
 #słownik={1:10, 'klucz':'wartosc', 1:20}
 #słownik['klucz']
 
-słownik={'key1':112244,'key2':335566} # utworzenie slownika
-print(słownik['key1'], słownik['key2']) # wyswietlenie calego słownika
-del słownik['key1'] #usuniecie key1
-print(słownik) #sprawdzenie czy usuniety
-słownik['key2'] = 'nowa wartosc' # nadanie nowej wartosci
-print(słownik) #sprawdzenie wartosci
+# słownik={'key1':112244,'key2':335566} # utworzenie slownika
+# print(słownik['key1'], słownik['key2']) # wyswietlenie calego słownika
+# del słownik['key1'] #usuniecie key1
+# print(słownik) #sprawdzenie czy usuniety
+# słownik['key2'] = 'nowa wartosc' # nadanie nowej wartosci
+# print(słownik) #sprawdzenie wartosci
+
+lista = [1, 2, 3, 4, 5, 6]
+print(lista)
+del lista[0]
+print(lista)
+lista[4] = 10
+print(lista)
+
 
 
 
