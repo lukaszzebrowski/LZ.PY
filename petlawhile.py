468549fad3fafc106b479369d290f13a49ba5ac3
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-02-27 14:22:53 +0100

    tryexcept

--- petlawhile.py
+++ petlawhile.py
@@ -4,10 +4,22 @@
 # else
 #     instrukcja po zakonczeniu dzialania petli
 
-z=0
+# z=0
+#
+# while z!=11:
+#     print(z)
+#     z+=1
+# else:
+#     print('Wyswietlono '+str(z)+' liczb')
 
-while z!=11:
-    print(z)
-    z+=1
-else:
-    print('Wyswietlono '+str(z)+' liczb')
+
+# lista1=[4,6,2,3,5,9,1]
+# lista2=[7,3,4,6]
+# suma=[]
+#
+# for a in lista1:
+#     for b in lista2:
+#         wynik=a+b
+#         suma.append(wynik)
+#     print(suma)
+
---
+++ tryexcept.py
@@ -0,0 +1,5 @@
+#obsluga bledow za pomoca skladni try except
+#try
+#-blok instruckji
+#except nazwa_bledu
+#-blok instrukcji po napotkaniu bledu
