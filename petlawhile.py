bd93182266b0db7981713e6716b8d6b45c0d51e4
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-02-27 13:51:08 +0100

    petla while

---
+++ petlawhile.py
@@ -0,0 +1,13 @@
+# petla while
+# while warunek:
+#     instrukcja lub blok instruckji
+# else
+#     instrukcja po zakonczeniu dzialania petli
+
+z=0
+
+while z!=11:
+    print(z)
+    z+=1
+else:
+    print('Wyswietlono '+str(z)+' liczb')
