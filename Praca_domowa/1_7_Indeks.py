6838ddfae91faff978400f378af06f22ca4d19c3
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-03-02 16:57:35 +0100

    1.7 Indeksy

--- Praca_domowa/1_7_Indeks.py
+++ Praca_domowa/1_7_Indeks.py
@@ -1,2 +1,5 @@
+#Do poszczególnych elementów łańcucha możemy się odwoływać przez podanie indeksu.
+# Np. pierwszy znak zapisany w zmiennej imie uzyskamy przez imie[0].
+# Zapisz dowolną zmienną łańcuchową i wyświetl jej drugą i ostatnią literę, wykorzystując indeksy.
 a = 'Konstantynopolitańczykowianeczka'
 print(a[2], a[-1:])
