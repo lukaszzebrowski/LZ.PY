b658798fa50e1e78088267d093c076d67726901e
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-03-02 16:20:31 +0100

    1.1 Typy danych

---
+++ Praca_domowa/1_1_typy_danych.py
@@ -0,0 +1,18 @@
+# Napisz pierwszy skrypt, w którym zadeklarujesz po dwie zmienne każdego typu a następnie wyświetl te zmienne
+
+a = 5
+b = 6
+c = 5.1
+d = 6.3
+e = 'Łukasz'
+f = 'Żebrowski'
+g = 2+10j
+h = 1+3j
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+print(e, type(e))
+print(f, type(f))
+print(g, type(g))
+print(h, type(h))
