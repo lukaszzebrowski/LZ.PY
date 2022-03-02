7811ab35f5fd2bd191b70baeec0c2e83d11064ea
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-03-02 16:49:55 +0100

    1.5 Capitalize

--- Praca_domowa/1_5_Capitalize.py
+++ Praca_domowa/1_5_Capitalize.py
@@ -1,3 +1,6 @@
+#Zapisz swoje imie i nazwisko w oddzielnych zmiennych wszystkie wielkimi literami.
+# Użyj odpowiedniej metody by wyświetlić je pisane tak, że pierwsza litera jest wielka a pozostałe małe.
+# (trzeba użyć metody capitalize)
 name = 'ŁUKASZ'
 surname = 'ŻEBROWSKI'
 
