62b086fa1174677248c338a692c256a12bc5f086
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-03-02 16:47:52 +0100

    1.6 Count

---
+++ Praca_domowa/1_6_Count.py
@@ -0,0 +1,30 @@
+a = """
+Lalala-lalala
+Lalala-lalala
+Lalala-lalala
+Lalala-lalala
+
+Jeśli chcesz z gardła kurz wypłukać
+Tu każdy wskaże ci drogę
+W bok od przystanku PeKaeSu
+W prawo od szosy asfaltowej
+Kuszą napisy ołówkiem kopiowym
+Na drzwiach, "Od dziesiątej otwarte"
+"Dziś polecamy kotlet mielony"
+I "lokal kategorii czwartej"
+Lej się chmielu
+Nieś muzyko po bukowym lesie
+Panna Zosia ma w oczach dwa nieba
+Trochę lata z nowej beczki przyniesie
+Lej się chmielu
+Nieś muzyko po bukowym lesie
+Panna Zosia ma w oczach dwa nieba
+Trochę lata z nowej beczki przyniesie
+
+Lalala-lalala
+Lalala-lalala
+Lalala-lalala
+Lalala-lalala
+"""
+
+print(a.count('la'))
