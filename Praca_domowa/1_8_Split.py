2bbe51bf48987f53b4c298fed9c1cfdc150c8f1a
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-03-02 17:03:30 +0100

    1.7 Indeksy

---
+++ Praca_domowa/1_8_Split.py
@@ -0,0 +1,6 @@
+#Zad.8 Zmienne łańcuchowe możemy dzielić wykorzystaj zmienną z
+# Zad. 6 i spróbuj ją podzielić na poszczególne wyrazy. (trzeba użyć metody split)
+
+a = 'Ala ma kota, a kot ma Alę'
+
+print(a.split(" "))
