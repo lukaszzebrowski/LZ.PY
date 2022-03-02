e094fe97ba2843e2736a8b7c7cfce02b7894ade0
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-03-02 16:48:52 +0100

    1.6 Count

--- Praca_domowa/1_6_Count.py
+++ Praca_domowa/1_6_Count.py
@@ -1,3 +1,5 @@
+#Napisz skrypt, gdzie w zmiennej string zapiszesz fragment tekstu piosenki z powtarzającymi się słowami np. „la la la”.
+# Następnie użyj odpowiedniej funkcji, która zliczy występowanie słowa „la”. (trzeba użyć metody count)
 a = """
 Lalala-lalala
 Lalala-lalala
