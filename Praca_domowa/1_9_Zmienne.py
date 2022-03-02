bc8afff159765688e84dc6d58fbdf1da3648ddbf
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-03-02 17:07:45 +0100

    1.9 Zmienne

---
+++ Praca_domowa/1_9_Zmienne.py
@@ -0,0 +1,9 @@
+#Zad.9 Napisz skrypt, w którym zadeklarujesz zmienne typu: string, float i szestnastkowe.
+# Następnie wyświetl je wykorzystując odpowiednie formatowanie.
+
+a = 0x1b6
+b = 1.86
+c = 'Łukasz'
+print(a)
+print(b)
+print(c)
