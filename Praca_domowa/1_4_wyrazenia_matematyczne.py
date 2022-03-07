e3d41d2a5b1c2faf79492c1b4e8ab17aa66023af
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-03-07 18:16:26 +0100

    1.4 wyrażenia matematyczne

--- Praca_domowa/1_4_wyrazenia_matematyczne.py
+++ Praca_domowa/1_4_wyrazenia_matematyczne.py
@@ -1,9 +1,9 @@
 #Napisz skrypt, który policzy i wyświetli następujące wyrażenia:
 
-import math as mt
+import math
 import numpy as np
 
 print(np.e**10)
-print((np.log(5+mt.sin(8)**2))**(1/6))
-print(mt.floor(3.55))
-print(mt.ceil(4.8))
+print((np.log(5+math.sin(8)**2))**(1/6))
+print(math.floor(3.55))
+print(math.ceil(4.8))
