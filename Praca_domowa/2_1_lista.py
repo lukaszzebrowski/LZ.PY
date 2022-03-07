c623d4289ffd672ee2aa23398467c5294779b3c8
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-03-07 11:25:30 +0100

    2.1 Lista

---
+++ Praca_domowa/2_1_lista.py
@@ -0,0 +1,10 @@
+#Napisz skrypt, w którym tworzysz listę ulubionych sportów,
+# odwróć ją a następnie dodaj mniej lubiane sporty na sam koniec.
+
+ulubione_sporty = ['Koszykówka', 'Siatkówka', 'Piłka Nożna', 'Piłka Ręczna']
+print(ulubione_sporty)
+ulubione_sporty.reverse()
+print(ulubione_sporty)
+ulubione_sporty.append('Skoki narciarskie')
+ulubione_sporty.append('E-sport')
+print(ulubione_sporty)
