b626f8628db2960d4ee12b5a75e3fa7867bc9118
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-03-07 18:07:52 +0100

    2.3 Słownik gry

---
+++ Praca_domowa/2_3_Slownik_gry.py
@@ -0,0 +1,10 @@
+#Stwórz słownik z ulubionymi grami komputerowymi.
+# Pomyśl, co może być kluczem a co wartością w takim słowniku.
+# Policz liczbę elementów w słowniku.
+
+dict_game = {'wn3': 'Wiedźmin 3', 'eg': 'Elden Ring', 'bn': 'Bloodborn',
+             'fy7': ' Final Fantasy 7', 'hs': 'Hades', 'ht': 'Horizon Forbidden West'}
+
+print(len(dict_game))
+print(dict_game.keys())
+print(dict_game.values())
