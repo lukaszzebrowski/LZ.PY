275e2685eb25e1872ff8adb1ee6ac670b225488b
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-03-07 14:46:08 +0100

    2.9 ERROR sqrt

---
+++ Praca_domowa/2_9_Error_sqrt.py
@@ -0,0 +1,14 @@
+#Napisz skrypt, który liczy pierwiastek z liczby podanej przez użytkownika
+# jeśli użytkownik poda wartość ujemną to powinien być wyłapany błąd.
+import math
+
+print('Policz pierwiastek z liczby: ')
+
+x = input('(Podaj liczbę i zatwierdź przyciskiem ENTER: ')
+x = int(x)
+
+try:
+    pierwiastek = math.sqrt(x)
+    print(pierwiastek)
+except ValueError:
+    print('\aBłąd!! Wprowadzono liczbę ujemną!!')
