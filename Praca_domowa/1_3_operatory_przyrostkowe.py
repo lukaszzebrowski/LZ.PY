f2c5586d787677eccf0d0a6e4669e46208c32e66
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-03-07 18:14:24 +0100

    1.3 operatory przyrostkowe

---
+++ Praca_domowa/1_3_operatory_przyrostkowe.py
@@ -0,0 +1,23 @@
+#Napisz skrypt, w którym stworzysz operatory przyrostkowe dla operacji: +, -, *, /, **, %
+
+a = 5
+print(a)
+
+a **= 2
+print(a)
+
+a += 10
+print(a)
+
+a -= 10
+print(a)
+
+a *= 4
+print(a)
+
+a /= 25
+print(a)
+
+a %= 3
+print(a)
+
