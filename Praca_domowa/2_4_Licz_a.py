b06590b7aee88ed753068d6dcf1403245f303007
lukaszzebrowski <luk.zebrowski@gmail.com>
2022-03-07 12:15:47 +0100

    2.4 Licz a

---
+++ Praca_domowa/2_4_Licz_a.py
@@ -0,0 +1,6 @@
+#Napisz skrypt, który pobiera od użytkownika zdanie
+# i liczy wystąpienia litery a. Użyj funkcji input
+
+licz_a = input('Napisz coś i zatwierdz przyciskiem "ENTER": ')
+
+print(licz_a.count('a'))
